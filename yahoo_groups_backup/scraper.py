0313759a45e92193184785e042114f369d7a4e53
hrenfroe <237295+hrenfroe@users.noreply.github.com>
2019-10-18 16:45:49 -0400

    Navigate the Yahoo login page for real this time

--- yahoo_groups_backup/scraper.py
+++ yahoo_groups_backup/scraper.py
@@ -47,14 +47,14 @@ class YahooBackupScraper:
         # email ...
         self.br.fill("username", self.login_email)
         time.sleep(1)
-        self.br.find_by_name("signin").click()
+        self.br.execute_script("document.getElementById('login-username-form').submit();")
         # Wait ...
         time.sleep(2)
 
         # password ...
         self.br.fill("password", self.password)
         time.sleep(1)
-        self.br.find_by_name("verifyPassword").click()
+        self.br.execute_script("document.getElementById('login-signin').click()")
         # Wait ...
         time.sleep(2)
 
